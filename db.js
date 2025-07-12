24cba7df2a9f449430202ae72380ed723f9b680b
Jefferson <guillot0777@gmail.com>
2025-07-12 15:30:51 -0500

    db fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT), 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
@@ -12,10 +12,10 @@ const conexion = mysql.createConnection({
 
 conexion.connect((err) => {
   if (err) {
-    console.error("Error al conectar a MySQL:", err);
+    console.error("❌ Error al conectar a MySQL:", err);
     return;
   }
-  console.log('Conectado a MySQL');
+  console.log('✅ Conectado a MySQL');
 });
 
 module.exports = conexion;
